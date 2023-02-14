6ce4c608db2b518f34979c5dcebe4d314100f600
Cyberod <110993874+Cyberod@users.noreply.github.com>
2023-02-14 10:30:37 +0100

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,34 +1,17 @@
 #!/usr/bin/python3
-"""
-Defines Place class
-"""
+"""Module for Place class."""
+
 from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """
-    Defines Place class
-    
-    initiation of Place that inherits from BaseModel class
-    Public Class Attributes:
-       (string) city_id: initialized as empty string
-       (string) user_id: initialized as empty string
-       (string) name: initialized as empty string
-       (string) description: initialized as empty string
-       (integer) number_rooms: intialized as zero
-       (integer) number_bathrooms: initialized as zero
-       (integer) max_guest: initialized as 0
-       (integer) price_by_night: initialized as 0
-       (float) latitude: initialized as 0.0
-       (float) longitude: initialized as 0.0
-       (list of strings) amenity_ids: initialized as empty list
-    """
+    """Class representing a Place."""
     city_id = ""
     user_id = ""
     name = ""
     description = ""
-    number_bathrooms = 0
     number_rooms = 0
+    number_bathrooms = 0
     max_guest = 0
     price_by_night = 0
     latitude = 0.0
